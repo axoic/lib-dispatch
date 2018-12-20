623e5fd0ccb39b81a9f8d2c5b05f1853a885590d
Jeff Wang <axoic@users.noreply.github.com>
2018-12-20 11:42:12 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,17 +1,22 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-LIBS_HOME=/home/build/app/libs
-TARGET_DIR=/home/build/app/dist
+REPO_HOME=`echo $(cd $(dirname $0); pwd)`
+LIBS_HOME=${REPO_HOME}/libs
+TARGET_DIR=${REPO_HOME}/dist
 
 if [ ! -d "${LIBS_HOME}" ]; then
     echo "Missing library folder"    
     exit 1
 fi
 
+if [ ! -d "${TARGET_DIR}" ]; then
+    mkdir -p ${TARGET_DIR}
+fi
+
 cd ${LIBS_HOME}/android
-zip -r dispatch-${TRAVIS_TAG}-android.zip .
+zip -r dispatch-android.zip .
 mv *.zip ${TARGET_DIR}
 
 cd ${LIBS_HOME}/linux
-zip -r dispatch-${TRAVIS_TAG}-linux.zip .
+zip -r dispatch-linux.zip .
 mv *.zip ${TARGET_DIR}
